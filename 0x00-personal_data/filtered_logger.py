ade4ebd0614f8fe0d678192d5cc00e2794a0472c
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-29 20:41:19 +0300

    Write a custom formatter class to format logged messages

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,8 +5,32 @@ Contains:
     =========
     filter_datum - Redactes sensitive information from log line
 """
+import logging
 import re
 from typing import List
+from datetime import datetime
+
+
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Custom formatter of the logged message"""
+        message = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            message, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str,
