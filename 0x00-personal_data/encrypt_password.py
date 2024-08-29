bc5d50ac8b98bcd8c7006a0898786d38da38993a
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-29 23:12:39 +0300

    Write a function to check if a given password matched a hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Generates a salted hash for the given password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if the given password matched against the hash"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
