bce809abe5d5c22e4cbd1439ad7690ee0874d97d
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-29 22:14:19 +0300

    Try another implementation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 """
-Contains:
-    Functions
-    =========
-    filter_datum - Redactes sensitive information from log line
+Definition of filter_datum function that returns an obfuscated log message
 """
+from typing import List
+import re
 import logging
 import os
-import re
-from typing import Sequence
-
 import mysql.connector
 
-PII_FIELDS = ("phone", "email", "name", "ssn", "password")
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Return an obfuscated log message
+    Args:
+        fields (list): list of strings indicating fields to obfuscate
+        redaction (str): what the field will be obfuscated to
+        message (str): the log line to obfuscate
+        separator (str): the character separating the fields
+    """
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
@@ -23,34 +36,38 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: Sequence[str]):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Custom formatter of the logged message"""
+        """
+        redact the message of LogRecord instance
+        Args:
+        record (logging.LogRecord): LogRecord instance containing message
+        Return:
+            formatted string
+        """
         message = super(RedactingFormatter, self).format(record)
-        return filter_datum(self.fields, self.REDACTION,
-                            message, self.SEPARATOR)
-
-
-def filter_datum(fields: Sequence[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """Redactes sensitive information from log line"""
-    for fld in fields:
-        message = re.sub("{}=.*?{}".format(fld, separator),
-                         "{}={}{}".format(fld, redaction, separator), message)
-    return message
+        redacted = filter_datum(self.fields, self.REDACTION,
+                                message, self.SEPARATOR)
+        return redacted
 
 
 def get_logger() -> logging.Logger:
-    """Returns custom logger"""
+    """
+    Return a logging.Logger object
+    """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.addHandler(stream_handler)
+
+    handler = logging.StreamHandler()
+
+    formatter = RedactingFormatter(PII_FIELDS)
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     return logger
 
 
@@ -61,14 +78,8 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
     host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    # conn = mysql.connector.connect(user=user,
-    #                                password=passwd,
-    #                                host=host,
-    #                                database=db_name)
-    conn = mysql.connector.MySQLConnection(
-        user=user,
-        password=passwd,
-        host=host,
-        database=db_name
-    )
+    conn = mysql.connector.connect(user=user,
+                                   password=passwd,
+                                   host=host,
+                                   database=db_name)
     return conn
