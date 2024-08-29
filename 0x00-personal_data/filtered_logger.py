2fc556f78ffd32839d470854db68858349f3b40e
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-29 17:32:11 +0300

    Shorten documentation to adhere to 5 lines limitation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,27 +3,14 @@
 Contains:
     Functions
     =========
-    filter_datum
+    filter_datum - Redactes sensitive information from log line
 """
 import re
 from typing import List
 
 
 def filter_datum(fields: List, redaction: str, message: str, separator: str):
-    """
-    Returns the given message/log line with the given fields redacted
-    using the provided redaction string
-
-    Args:
-        fields (list): A list of the fields whose values need to be redacted
-        from the message
-        redaction (str): The string to be used to redact the field values
-        message (str): The log line
-        separator (str): The string separating the fields in the log line
-
-    Returns:
-        (str): The obfuscated/redacted message/log line
-    """
+    """Redactes sensitive information from log line"""
     for field in fields:
         message = re.sub(r"({}=)(.*?){}".format(field, separator),
                          r"\1{}{}".format(redaction, separator), message)
