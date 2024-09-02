32af7b420a30fb25c6ae111811b35c20b8f2831e
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-02 21:32:21 +0300

    Write a method to extract the encoded part of the Authorization header value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,11 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Implements Basic Authentication"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extracts the encoded part of the 'Authorization' header
+        value"""
+        if authorization_header and isinstance(authorization_header, str):
+            if authorization_header.startswith("Basic "):
+                return authorization_header.removeprefix("Basic ")
+        return None
