fbc4eee94dbe703232c95702704d048c169bc31d
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-02 21:46:37 +0300

    Write a method for decoding a base64 encoded bytes object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ Contains:
     BasicAuth - Inherits from Auth and implements
     basic authentication
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -18,4 +19,15 @@ class BasicAuth(Auth):
         if authorization_header and isinstance(authorization_header, str):
             if authorization_header.startswith("Basic "):
                 return authorization_header[6:]
+        return None
+
+    def decode_base64_authorization_header(self,
+                                           b64_auth_header: str) -> str:
+        """Decodes the base64 encoded authorization header value"""
+        if b64_auth_header:
+            try:
+                b64_decoded = base64.b64decode(b64_auth_header)
+            except Exception:
+                return None
+            return b64_decoded.decode("utf-8")
         return None
