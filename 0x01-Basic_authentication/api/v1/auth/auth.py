ab45b87221bd729492dfc1211a9bffa9d39ed3b1
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-02 20:34:28 +0300

    Fix pycodestyle issues

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,11 +14,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user"""
         return None
