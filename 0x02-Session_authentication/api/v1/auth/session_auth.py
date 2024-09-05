1a9f839eb5a9d9a0be7376ebeaca9df9ebe0025a
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-05 13:36:19 +0300

    Write a method to create a session for users by their IDs

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,9 +5,18 @@ Contains:
     =======
     SessionAuth - Session authentication
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Implements session authentication"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a new session for users"""
+        if user_id and isinstance(user_id, str):
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
