53ce3c6ca04957afee81456ed66284600df21c19
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-05 18:47:21 +0300

    Write an implementation of the session authentication system that saves sessions to a db instead of memory

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Contains:
+    Classes
+    =======
+    SessionDBAuth - Session authentication with sessions stored in
+    a database
+"""
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+
+
+class SessionDBAuth(SessionExpAuth):
+    """Session authentication with database"""
+    def create_session(self, user_id=None):
+        """Creates session and stores it in a database"""
+        session_id = super().create_session(user_id)
+        if session_id:
+            session = UserSession(user_id=user_id, session_id=session_id)
+            session.save()
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """
+        Returns the user id associated with the given
+        session id
+        """
+        if super().user_id_for_session_id(session_id):
+            try:
+                sessions = UserSession.search({"session_id": session_id})
+            except Exception:
+                return None
+            if sessions:
+                return sessions[0].user_id
+        return None
+
+    def destroy_session(self, request=None):
+        if request:
+            session_id = self.session_cookie(request)
+            if session_id:
+                user_id = self.user_id_for_session_id(session_id)
+                if user_id:
+                    try:
+                        sessions = UserSession.search({"session_id": session_id})
+                    except Exception:
+                        return False
+                    if sessions:
+                        sessions[0].remove()
+                        return True
+        return False
+
