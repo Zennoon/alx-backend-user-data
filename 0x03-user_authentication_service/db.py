1e76249fb680f6454d977ddcd156a4450a68a8b3
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-10 18:27:07 +0300

    Further shorten method by using the filter_by method which accepts a dictionary

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,10 +3,9 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from typing import Dict, List
 
 from user import Base, User
 
@@ -42,12 +41,10 @@ class DB:
         session.commit()
         return user
 
-    def find_user_by(self, *args, **kwargs):
+    def find_user_by(self, *args: List, **kwargs: Dict) -> User:
         """
         Filters users by given keyword args and returns result
         if found, else raise an exception
         """
         users = self._session.query(User)
-        for key, val in kwargs.items():
-            users = users.filter(User.__dict__.get(key) == val)
-        return users.one()
+        return users.filter_by(**kwargs).one()
