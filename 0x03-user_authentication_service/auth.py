09f36c8b5a4dfcf1e354ad98a68ea01ec4078422
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-10 18:55:29 +0300

    Write an Auth class to serve as the interface to authentication of users

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,7 +7,32 @@ Contains:
     using the bcrypt.hashpw function
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        """Initialize a new instance"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User  :
+        """
+        Registers a new user into the database after configurations
+        and checks are performed
+        """
+        try:
+            _ = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_pwd = _hash_password(password)
+            return self._db.add_user(email, hashed_pwd)
+        else:
+            raise ValueError("User {} already exists".format(email))
