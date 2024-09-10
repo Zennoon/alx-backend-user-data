5807531dbe1deb3bfd716ef5a0c17dd8e4176257
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-10 17:33:10 +0300

    Change the import source of the declarative base function

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,7 +10,7 @@ from sqlalchemy import (
     Integer,
     String
 )
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 base = declarative_base()
 
