e317b5530513f869b1a75180dd8a5f5dc65f67d2
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-10 18:10:44 +0300

    Write a method to retrieve User objects/records by attributes

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,6 +3,8 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
@@ -39,3 +41,18 @@ class DB:
         session.add(user)
         session.commit()
         return user
+
+    def find_user_by(self, *args, **kwargs):
+        """Filters users by given keyword args and returns result
+        if found, else raise an exception"""
+        users = self._session.query(User)
+        try:
+            for key, val in kwargs.items():
+                users = users.filter(User.__dict__.get(key) == val)
+            users = users.all()
+        except InvalidRequestError:
+            raise InvalidRequestError
+        try:
+            return users[0]
+        except IndexError:
+            raise NoResultFound
