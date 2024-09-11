533845190dedc1023abde64f0f538e8c40128e42
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-11 16:22:36 +0300

    Write a route handler to allow users to login and create a session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,7 +5,7 @@ Contains:
     ============
     app - A flask application
 """
-from flask import Flask, jsonify, request
+from flask import abort, Flask, jsonify, request
 
 from auth import Auth
 
@@ -21,7 +21,8 @@ def index():
 
 @app.route("/users", methods=["POST"])
 def users():
-    """Handles POST requests the the /users route"""
+    """Handles POST requests to the /users route.
+    Handles user registration"""
     email = request.form.get("email")
     password = request.form.get("password")
     try:
@@ -29,6 +30,20 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": user.email, "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """Handles POST requests to the /sessions route
+    Handles user login and session creation"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        resp = jsonify({"email": email, "message": "logged in"})
+        resp.set_cookie("session_id", session_id)
+        return resp
+    abort(401)
 
 
 if __name__ == "__main__":
